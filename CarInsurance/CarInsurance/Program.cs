bfee235c13b7be4d8804d46197e8656b907c79ee
Braydon <burrow.braydon14@gmail.com>
2020-05-29 11:21:32 -0600

    Assignment 101

---
+++ CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int curAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + curAge);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI? please enter 'true' or 'false' ");
+            bool hadDui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Your answer is: " + hadDui);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int curTicket = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Current speeding tickets are: " + curTicket);
+            Console.ReadLine();
+
+            Console.WriteLine("Are you qualified for insurance?");
+            bool olderThan15 = curAge >= 15;
+            bool noDui = hadDui == false;
+            bool threeOrLess = curTicket <= 3;
+            Console.WriteLine(!(!olderThan15 || !noDui || !threeOrLess));
+            Console.ReadLine();
+
+
+        }
+    }
+}
