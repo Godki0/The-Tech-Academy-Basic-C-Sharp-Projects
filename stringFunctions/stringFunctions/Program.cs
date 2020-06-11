ae2581529392fe5b46d654a1baf2f6e9881c9f68
Braydon <burrow.braydon14@gmail.com>
2020-06-10 19:16:25 -0600

    Assignment 125 plus test code

---
+++ stringFunctions/stringFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Braydon";
+            //string quote = "The man said, \"Hello\", Braydon. \n Hello on a new line. \n \t Hello on a tab.";
+            //string filename = @"C:\user\bur"; // @ sign stats all after has no escape sequence. 
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper(); // name.ToLower();
+
+            //Console.WriteLine(quote);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Braydon");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ stringTest/stringTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sOne = "Hey there, ";
+            string sTwo = "how are you? ";
+            string sThree = "My name is Braydon ";
+
+            sOne = sOne.ToUpper();
+
+
+            Console.WriteLine(sOne + sTwo + sThree);
+            Console.ReadLine();
+            
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Braydon");
+            sb.Append("\nit is very nice to meet you!");
+            sb.Append("\nPlease don't be shy.");
+            sb.Append("\nTell me more about yourself?");
+            sb.Append("\nWell thank you for your time!");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
