f5eaa0c0ec6fb498dc20053e24473c59fa8e05f8
Braydon <burrow.braydon14@gmail.com>
2020-06-14 17:37:41 -0600

    Assignment 161 plus test code

---
+++ methodApp1/methodApp1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp1
+{
+    public class Class1
+    {
+        public static void Addition()
+        {
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int result = 5 + userInput;
+            Console.WriteLine("Your answer is : " + result);
+            Console.ReadLine();
+        }
+
+        public static void Multiply()
+        {
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int result = 5 * userInput;
+            Console.WriteLine("Your answer is : " + result);
+            Console.ReadLine();
+        }
+
+        public static void Divide()
+        {
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int result = 5 / userInput;
+            Console.WriteLine("Your answer is : " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodApp1/methodApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            Class1.Addition();
+            Console.WriteLine("Please enter a number.");
+            Class1.Multiply();
+            Console.WriteLine("Please enter a number.");
+            Class1.Divide();
+
+        }   
+    }
+}
