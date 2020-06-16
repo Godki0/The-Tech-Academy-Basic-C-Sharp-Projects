cd9cf6a82c7e45cd25caa59a4e467c8f85cf76d0
Braydon <burrow.braydon14@gmail.com>
2020-06-15 21:53:29 -0600

    Assignment 166

---
+++ methodApp166/methodApp166/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methodMath math = new methodMath();
+            int v1 = math.addMath(5,5);
+            Console.WriteLine(v1);
+            Console.ReadLine();
+        }
+    }
+}
