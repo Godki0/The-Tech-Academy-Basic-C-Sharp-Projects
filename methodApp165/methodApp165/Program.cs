cc28b4b22a8b2a6eb62e47cb2082ac41cfd3834f
Braydon <burrow.braydon14@gmail.com>
2020-06-15 21:11:54 -0600

    Assignment 165

---
+++ methodApp165/methodApp165/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathMethod math1 = new mathMethod();
+            int userInput, userInput2;
+            int result;
+            Console.WriteLine("Please enter one number!");
+            if (!int.TryParse(Console.ReadLine(), out userInput))
+                throw new ArgumentException("No valid number input");
+
+            Console.WriteLine("Please enter a second number! Not required to work");
+            if (int.TryParse(Console.ReadLine(), out userInput2))
+                result = math1.mathAdd(userInput, userInput2);
+            else
+                result = math1.mathAdd(userInput);
+
+            Console.WriteLine("Your answer is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodApp165/methodApp165/mathMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace methodApp165
+{
+    class mathMethod
+    {
+        public int mathAdd(int x, [ Optional ] int y)
+        {
+            int result = x + y;
+            return result;
+        }
+    }
+}
