ad0f1a19497660677637ae57e963dc2864bbd2e1
Braydon <burrow.braydon14@gmail.com>
2020-06-18 14:07:12 -0600

    Assignment 182

---
+++ methodApp182/methodApp182/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp182
+{
+    class Employee : Person, Iquittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I quit");
+        }
+    }
+}
