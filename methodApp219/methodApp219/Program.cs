2bbcbba4f6dbe188b72f555c56a7e2ec88020e49
Braydon <burrow.braydon14@gmail.com>
2020-06-22 11:09:36 -0600

    Assignment 219

---
+++ methodApp219/methodApp219/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace methodApp219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\burro\Logs\log1.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+            Console.WriteLine("Here is your number:" + userInput);
+            Console.ReadLine();
+        }
+    }
+}
