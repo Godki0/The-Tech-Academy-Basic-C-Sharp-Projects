9bb7e01d14d4ab5485bc88cf621110e6d2246a48
Braydon <burrow.braydon14@gmail.com>
2020-06-22 11:34:07 -0600

    Assignment 222

---
+++ methodApp222/methodApp222/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp222
+{
+    class Program
+    {
+        private static DateTime x;
+        private static DateTime y;
+
+        static void Main(string[] args)
+        {
+            x = DateTime.Now;
+            Console.WriteLine(x);
+            Console.WriteLine("Please enter a number!");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            y = x.AddHours(userInput);
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
