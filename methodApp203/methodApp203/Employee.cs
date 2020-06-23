849c1dc572f3132ac4bb74d48df3969f7cd76e4e
Braydon <burrow.braydon14@gmail.com>
2020-06-23 13:06:34 -0600

    Assignment 203

---
+++ methodApp203/methodApp203/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp203
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public List<string> employees { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ methodApp203/methodApp203/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> employees = new List<string>() { "Joe Can" , "Bob Bird", "Jerry Join", "Jim Don", "Johnny Main", "Jeff Fort", "Marry Grau", "Steve Scar", "Berry Bee", "Joe Can" };
+            List<string> workers = new List<string>();
+            foreach (string name in employees)
+            {
+                if (name == "Joe Can")
+                {
+                    workers.Add(name);
+                    Console.WriteLine(name);
+                }
+            }
+
+            List<string> work = employees.Where(x => x.StartsWith("Joe")).ToList();
+            foreach (string name in work)
+            {
+                Console.WriteLine(name);
+            }
+
+            List<int> Id = employees.FindIndex(x => x >= 5).ToList();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
