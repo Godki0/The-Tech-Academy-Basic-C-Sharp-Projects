be7a2c07d1deb94bb8fc36f901e2bc5e7340f4fa
Braydon <burrow.braydon14@gmail.com>
2020-06-23 11:53:56 -0600

    Assignment 190

---
+++ methodApp190/methodApp190/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp190
+{
+    class Employee<T>
+    {
+        private List<T> _things = new List<T>();
+        public List<T> things { get { return _things; } set { _things = value; } }
+    }
+}
---
+++ methodApp190/methodApp190/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodApp190
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            Employee<int> employee1 = new Employee<int>();
+            employee.things.Add("john");
+            employee.things.Add("Marry");
+            employee1.things.Add(1);
+            employee1.things.Add(2);
+
+            foreach (string employ in employee.things)
+            {
+                Console.WriteLine(employ);
+            }
+            foreach (int employ1 in employee1.things)
+            {
+                Console.WriteLine(employ1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
